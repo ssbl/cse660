08f21afd4fff593fbd60aae5f7dafc1b37542ccb
Shubham Lagwankar <shubhu105@gmail.com>
2017-09-10 15:57:32 -0400

    fix whitespace, remove some print()s/comments

--- assignment1/attack.py
+++ assignment1/attack.py
@@ -13,56 +13,44 @@ def sum_query_noisy(D, rows, E):
     return sum_query(D, rows) + random.uniform(0,E)
 
 def subset(my_list):
-	result = []
-	for i in range(1,len(my_list)+1):
-		result += list(itertools.combinations(my_list,i))
-	return result
+        result = []
+        for i in range(1,len(my_list)+1):
+                result += list(itertools.combinations(my_list,i))
+        return result
 
 def binary_set(n):
-	return list(itertools.product([0,1], repeat=n))
+        return list(itertools.product([0,1], repeat=n))
 
+# https://stackoverflow.com/a/31007358
 def hamming2(s1, s2):
     """Calculate the Hamming distance between two bit strings"""
     assert len(s1) == len(s2)
     return sum(c1 != c2 for c1, c2 in zip(s1, s2))
 
 def attack(D,E):
-	n = len(D)
-	noisy_query_result = {}
-	for i in subset(range(0,n)):
-		nq = sum_query_noisy(D,i,E)
-		noisy_query_result[i] = nq
-	# print (noisy_query_result)
+    n = len(D)
+    noisy_query_result = {}
+    for i in subset(range(0,n)):
+        nq = sum_query_noisy(D,i,E)
+        noisy_query_result[i] = nq
 
-	D_dash = binary_set(n)
-	print("D_dash")
-	# print(D_dash)
-	print("attack")
-	for d in D_dash:
-		found = True
-		for i in subset(range(0,n)):
-			sq = sum_query(d, i)
-			nqr = noisy_query_result[i]
-			# print(d,i,abs(sq-nqr), sq,nqr)
-			if abs(sq-nqr) > E:
-				found = False
-				break
-		if found:
-			return d
-
+    D_dash = binary_set(n)
+    for d in D_dash:
+        found = True
+        for i in subset(range(0,n)):
+            sq = sum_query(d, i)
+            nqr = noisy_query_result[i]
+            if abs(sq-nqr) > E:
+                found = False
+                break
+        if found:
+            return d
 
 if __name__ == '__main__':
-    D = create_dataset(12)
-    # D = [1,1,1,1,1,1,1,1,1,1]
+    n = 6
     E = 3
-    print(D)
-    # print(sum_query(D, [3, 5, 9]))
-    # print(sum_query_noisy(D,[3,5,9],1))
-    # print(subset([1,2,3,4]))
-    # print(binary_set(4))
-
     for i in range(100):
-    	D = create_dataset(12)
-    	D_dash =  attack(D,E)
-    	hamDist = hamming2(D_dash,D)
-    	print(D,D_dash,hamDist)
+        D = create_dataset(n)
+        D_dash =  attack(D,E)
+        hamDist = hamming2(D_dash,D)
+        print(D,D_dash,hamDist)
