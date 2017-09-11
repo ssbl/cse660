4d19e380952b1a45ef2ec42ec60a2ae04f00c76d
Shubham Lagwankar <shubhu105@gmail.com>
2017-09-11 08:24:41 -0400

    use correct value in random.uniform

--- assignment1/attack.py
+++ assignment1/attack.py
@@ -4,22 +4,22 @@ import itertools
 
 def create_dataset(n):
     assert n >= 0
-    return np.random.random_integers(0, 1, n)
+    return np.random.random_integers(0, 1, n).tolist()
 
 def sum_query(D, rows):
-    return np.sum([D[i] for i in rows])
+    return sum([D[i] for i in rows])
 
 def sum_query_noisy(D, rows, E):
-    return sum_query(D, rows) + random.uniform(0,E)
+    return sum_query(D, rows) + random.uniform(-E, E)
 
 def subset(my_list):
-        result = []
-        for i in range(1,len(my_list)+1):
-                result += list(itertools.combinations(my_list,i))
-        return result
+    result = []
+    for i in range(1,len(my_list)+1):
+        result += list(itertools.combinations(my_list,i))
+    return result
 
 def binary_set(n):
-        return list(itertools.product([0,1], repeat=n))
+    return list(itertools.product([0,1], repeat=n))
 
 # https://stackoverflow.com/a/31007358
 def hamming2(s1, s2):
@@ -36,19 +36,17 @@ def attack(D,E):
 
     D_dash = binary_set(n)
     for d in D_dash:
-        found = True
-        for i in subset(range(0,n)):
+        for i in noisy_query_result.keys():
             sq = sum_query(d, i)
             nqr = noisy_query_result[i]
             if abs(sq-nqr) > E:
-                found = False
                 break
-        if found:
+        else:
             return d
 
 if __name__ == '__main__':
-    n = 6
-    E = 3
+    n = 8
+    E = 1
     for i in range(100):
         D = create_dataset(n)
         D_dash =  attack(D,E)
