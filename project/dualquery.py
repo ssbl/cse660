92e765c49ddd3c9fbd58c7ec7725cca26f59e741
Shubham Lagwankar <shubhu105@gmail.com>
2017-11-19 13:53:39 -0500

    use pulp's solver instead of scipy.optimize.linprog.

--- project/dualquery.py
+++ project/dualquery.py
@@ -1,8 +1,9 @@
 import itertools
 
+import pulp
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import linprog
+
 
 def create_dataset(n,nbits):
     columns = []
@@ -50,13 +51,17 @@ if __name__ == '__main__':
     beta = 0.05
     delta = np.exp(-10)
     alpha = 2.5
-    steps = (16 * np.log(len(Q))) / alpha**2
-    eta = alpha / 4
-    samples = (48 * np.log(2 * (2**nbits) * steps / beta)) / alpha**2
+    # steps = (16 * np.log(len(Q))) / alpha**2
+    # eta = alpha / 4
+    # samples = (48 * np.log(2 * (2**nbits) * steps / beta)) / alpha**2
+    steps = 14
+    eta = 3.0
+    samples = 20
     print('steps =', steps, 'eta =', eta, 'samples =', samples)
 
     # for t in range(steps):
-    sampled_queries = sample_queries(Q, Qdist, samples)
+    sampled_queries = np.array(sample_queries(Q, Qdist, samples))
+
     npositive = nnegative = 0
     for query in sampled_queries:
         if query[0]:
@@ -65,38 +70,28 @@ if __name__ == '__main__':
             npositive += 1
     print('positive =', npositive, 'negative =', nnegative)
 
-    matrix = []
-    rhs_vector = []
-    count_pos, count_neg = 1, 1
+    model = pulp.LpProblem('Dual Query', pulp.LpMaximize)
+    x = np.array([pulp.LpVariable('x' + str(i), cat='Binary') for i in range(nbits)])
+    c = np.array([pulp.LpVariable('c' + str(i), cat='Binary') for i in range(npositive)])
+    d = np.array([pulp.LpVariable('d' + str(i), cat='Binary') for i in range(nnegative)])
+
+    model += sum(c) + sum(d), 'Objective function'
+
+    countp = countn = 0
     for query in sampled_queries:
         if not query[0]:
-            equation = query[1:] + ([0] * (count_pos - 1)) + [-3] \
-                       + ([0] * (npositive - count_pos)) + \
-                       ([0] * nnegative)
-            count_pos += 1
-            rhs_vector.append(0)
-            matrix.append(equation)
+            model += np.dot(query[1:], x) - 3 * c[countp] >= 0
+            countp += 1
         else:
-            equation = [x*-1 for x in query[1:]] + ([0] * npositive) + \
-                       ([0] * (count_neg - 1)) + [-1] + \
-                       ([0] * (nnegative - count_neg))
-            rhs_vector.append(-3)
-            count_neg += 1
-            matrix.append(equation)
+            model += np.dot(query[1:], -x) - d[countn] >= -3
+            countn += 1
+    model.solve()
 
-    rhs_vector = -np.array(rhs_vector)
-    matrix = -np.array(matrix)
-    print(rhs_vector)
-    print(sampled_queries[1])
-    print(matrix[1])
-
-    c = ([0] * nbits) + ([-1] * int(samples))
-    A = matrix
-    b = rhs_vector
-    bounds_x = [[0, 1] for _ in range(nbits)]
-    bounds_d = [[0, 3] for _ in range(nnegative)]
-    bounds_c = [[0, 1] for _ in range(npositive)]
-    bounds_vector = bounds_x + bounds_c + bounds_d
-    res = linprog(c, A_ub=A, b_ub=rhs_vector, bounds=(bounds_vector),
-                  options={'disp': True})
-    print(res)
+    for xvar in x:
+        print(xvar.varValue)
+    print('=== c ===')
+    for cvar in c:
+        print(cvar.varValue)
+    print('=== d ===')
+    for dvar in d:
+        print(dvar.varValue)
