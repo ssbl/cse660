3ea5f409720afde566a4d9aec3b4beccb66b96f9
Shubham Lagwankar <shubhu105@gmail.com>
2017-11-26 08:23:28 -0500

    project: files as command line arg, plot results from multiple files.

--- project/parseplot.py
+++ project/parseplot.py
@@ -1,23 +1,25 @@
 import json
+from sys import argv, stderr
 
 import matplotlib.pyplot as plt
 
-def parse(path):
+def parse(pathlist):
     epsilon = []
     max_error = []
     avg_error = []
 
-    with open(path) as log:
-        for line in log:
-            data = json.loads(line)
-            max_error.append(data['max_error'])
-            avg_error.append(data['avg_error'])
-            T = data['steps']
-            s = data['samples']
-            eta = data['eta']
-            n = data['n']
-            e = T * (T - 1) * s * eta / n
-            epsilon.append(e)
+    for path in pathlist:
+        with open(path) as log:
+            for line in log:
+                data = json.loads(line)
+                max_error.append(data['max_error'])
+                avg_error.append(data['avg_error'])
+                T = data['steps']
+                s = data['samples']
+                eta = data['eta']
+                n = data['n']
+                e = T * (T - 1) * s * eta / n
+                epsilon.append(e)
 
     return epsilon, max_error, avg_error
 
@@ -26,5 +28,11 @@ def plot_error(epsilon, error):
     plt.show()
 
 if __name__ == '__main__':
-    epsilon, max_error, avg_error = parse('log.json')
-    plot_error(epsilon, avg_error)
+    argc = len(argv)
+    if argc <= 1:
+        print('usage: parseplot <path-to-result>...', file=stderr)
+        exit(1)
+
+    files = argv[1:]
+    epsilon, max_error, avg_error = parse(files)
+    plot_error(epsilon, max_error)
