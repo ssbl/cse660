d9ba23eb69df4c7916300f6df75180c97d4440c9
Shubham Lagwankar <shubhu105@gmail.com>
2017-11-29 08:51:38 -0500

    fix range in ls.

--- assignment11/ls.py
+++ assignment11/ls.py
@@ -7,19 +7,20 @@ References: Salil Vadhan notes section 3.1
 import numpy as np
 import matplotlib.pyplot as plt
 
-def ls(D, q):
-    # Here D is of the form {1..10}^n
+def ls(D, q, lowerbound, upperbound):
+    n = len(D)
+    # Here D is of the form {lower..upper}^n
     neighbors = []
-    for i in range(len(D)):
-        for x in range(3):
-            if x == D[i]:
-                continue
-            neighbor = D[:]         # create a copy of D
-            neighbor[i] = x
-            neighbors.append(neighbor)
+    for i in range(n):
+        for x in range(lowerbound, upperbound + 1):
+            if x != D[i]:
+                neighbor = D.copy()
+                neighbor[i] = x # change a single row
+                neighbors.append(neighbor)
 
     maxdiff = 0
     for neighbor in neighbors:
+        diff = abs(q(D) - q(neighbor))
         maxdiff = max(maxdiff, abs(q(D) - q(neighbor)))
 
     return maxdiff
@@ -29,29 +30,37 @@ def lapmech(D, qD, e, sens):
     return qD + np.random.laplace(scale=b)
 
 if __name__ == '__main__':
-    # Run queries on random datasets of size 50..1000 in steps of 50.
+    # Run queries on random datasets of size 50..500 in steps of 50.
     # Compute the error for both approaches.
-    nlist = list(range(50, 1001, 50))
+    lowerbound, upperbound = 1, 10
+    nlist = list(range(50, 501, 50))
     queries = [np.median, np.average]
     epsilon = 1.0
 
     error_gs = []
     error_ls = []
 
+    xs = []
+    elemrange = upperbound - lowerbound
     for n in nlist:
-        D = np.random.random_integers(1, 10, n).tolist()
+        D = np.random.random_integers(lowerbound, upperbound, n).tolist()
         for query in queries:
-            query_desc = 'median' if query is np.median else 'average'
+            xs.append(n)
+            if query is np.median:
+                query_desc = 'median'
+            else:
+                query_desc = 'average'
             qD = query(D)
-            lsval = ls(D, query)
-            gs = 10 if query is np.median else 10 / n
+            lsval = ls(D, query, lowerbound, upperbound)
+            gs = elemrange if query is np.median else elemrange / n
             print('Running LapMech for n = {}, query = {}, LS = {}'.format(
                 n, query_desc, lsval))
             error_gs.append(abs(qD - lapmech(D, qD, epsilon, gs)))
             error_ls.append(abs(qD - lapmech(D, qD, epsilon, lsval)))
 
-    # Error when using LS turns out to be very low.
-    plt.plot(error_gs, 'ro', label='global')
-    plt.plot(error_ls, 'go', label='local')
+    plt.plot(xs, error_gs, 'ro', label='global')
+    plt.plot(xs, error_ls, 'go', label='local')
+    plt.xlabel('number of rows')
+    plt.ylabel('error')
     plt.legend()
     plt.show()
